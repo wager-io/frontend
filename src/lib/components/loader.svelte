9fd8baa6f7835740980ba67d63f65e37e74aa324
highscoretech <highscoretechng@gmail.com>
2024-04-30 17:52:45 -0600

    loading color

--- src/lib/components/loader.svelte
+++ src/lib/components/loader.svelte
@@ -1,5 +1,5 @@
 <div class="sc-gKclnd sc-furwcr keuypW hTWjOR loading">
-	<svg viewBox="0 0 84 24" fill="#5da000"
+	<svg viewBox="0 0 84 24" fill="#9900CC"
 		><circle cx="18" cy="12" r="6"></circle>
 		<circle cx="18" cy="12" r="6"></circle>
 		<circle cx="42" cy="12" r="6"></circle>
